feda9a0c64cf2f8f5d975f9118e1608fde5f0832
andooo26 <yota426513@gmail.com>
2025-09-05 12:59:18 +0900

    devicesを取得しjsonへ格納

--- sb.sh
+++ sb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 URL="https://api.switch-bot.com/v1.0"
+ENV_FILE=".env"
+DEVICES_FILE="devices.json"
 
 # .envよりAPI_KEY
-if [ -f .env ]; then
-  export $(cat .env | xargs)
+if [ -f "$ENV_FILE" ]; then
+  export $(cat "$ENV_FILE" | xargs)
 else
   exit 1
 fi
@@ -23,10 +25,7 @@ case "$command" in
   # listコマンド
   list)
     case "$subcommand" in
-      deviceall)
-        curl -s -X GET "$URL/devices" \
-          -H "Authorization: $API_KEY"
-        ;;
+
       *)
         echo "正しい引数を指定してください"
         exit 1
@@ -34,19 +33,23 @@ case "$command" in
     esac
     ;;
 
-  #setコマンド
+  # setコマンド
   set)
     case "$subcommand" in
-    key)
-      read -p "Enter your API_KEY: " NEW_KEY
-      echo "API_KEY=$NEW_KEY" > ".env"
-      echo "API_KEY を $ENV_FILE に保存しました"
-      exit 0
-      ;;
-    *)
-      echo "正しい引数を指定してください"
-      exit 1
-      ;;
+      key)
+        read -p "Enter your API_KEY: " NEW_KEY
+        echo "API_KEY=$NEW_KEY" > "$ENV_FILE"
+        echo "API_KEY を $ENV_FILE に保存しました"
+        exit 0
+        ;;
+      devices)
+        curl -s -X GET "$URL/devices" \
+        -H "Authorization: $API_KEY" | jq '.body.deviceList |= map(. + {unique: ""}) | .body.infraredRemoteList |= map(. + {unique: ""})' | tee "$DEVICES_FILE"
+        ;;
+      *)
+        echo "正しい引数を指定してください"
+        exit 1
+        ;;
     esac
     ;;
 esac
